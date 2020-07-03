899ee12e90eebc6a0b865c063677b5bd1e3abc67
usaghir <umer.saghir@live.com>
2020-07-04 00:44:45 +0200

    Added the server setup and basic database and rest functionalities using Express.

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'Random001',
+  host: 'localhost',
+  port: 5432,
+  database: 'pizzatask',
+});
+
+module.exports = pool;
