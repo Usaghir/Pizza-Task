9b8985ae1470a4f3aa977364868d9bcef383e247
usaghir <umer.saghir@live.com>
2020-08-19 12:45:56 +0200

    Added more css Home Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,35 +4,37 @@ import './Component.css';
 
 function Home() {
   return (
-    <div className="container pt-5 mt-5 d-flex justify-content-center g-font">
-      <div class="card w-75 ">
-        <div class="card-body main-left" style={{ backgroundImage: `url(${MainTopLeft})` }}>
-          <h5 class="main-left">HIGH QUALITY INGREDIENTS</h5>
-          <h3 class="">HEALTHY & DELICIOUS</h3>
-          <p class="card-text">Will be with you with just a click of a button.</p>
-          <a href="#" class="btn btn-primary">
-            Choose
-          </a>
+    <div className="mt-5 pt-5">
+      <div className="container pt-5 mt-5 d-flex justify-content-center g-font">
+        <div class="card w-75 ">
+          <div class="card-body main-left" style={{ backgroundImage: `url(${MainTopLeft})` }}>
+            <h5 class="main-left">HIGH QUALITY INGREDIENTS</h5>
+            <h3 class="">HEALTHY & DELICIOUS</h3>
+            <p class="card-text">Will be with you with just a click of a button.</p>
+            <a href="#" class="btn btn-primary">
+              Choose
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div class="pl-3 w-25">
-        <div class="card w-100 ">
-          <div class="card-body">
-            <h5 class="card-title">Fast Delivery</h5>
-            <p class="card-text">Get the delivery in your door step with in an hour.</p>
+        <div class="pl-3 w-25 ">
+          <div class="card w-100 ">
+            <div class="card-body">
+              <h5 class="card-title">Fast Delivery</h5>
+              <p class="card-text">Get the delivery in your door step with in an hour.</p>
+            </div>
           </div>
-        </div>
-        <div class="card w-100 ">
-          <div class="card-body">
-            <h5 class="card-title">Bulk Orders</h5>
-            <p class="card-text">Customers can book orders for events and ceremonies.</p>
+          <div class="card w-100 ">
+            <div class="card-body">
+              <h5 class="card-title">Bulk Orders</h5>
+              <p class="card-text">Customers can book orders for events and ceremonies.</p>
+            </div>
           </div>
-        </div>
-        <div class="card w-100 ">
-          <div class="card-body">
-            <h5 class="card-title">Free Delivery</h5>
-            <p class="card-text">Free delivery for customers on all orders above 100.</p>
+          <div class="card w-100 ">
+            <div class="card-body">
+              <h5 class="card-title">Free Delivery</h5>
+              <p class="card-text">Free delivery for customers on all orders above 100.</p>
+            </div>
           </div>
         </div>
       </div>
